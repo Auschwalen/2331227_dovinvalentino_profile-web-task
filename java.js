128b21459a786c7ae076ae93a90399a430c589fb
Auschwalen <71735083+Auschwalen@users.noreply.github.com>
2024-03-25 14:25:10 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+function large(img) {
+    img.style.padding = "1.5vw 3vw"
+    img.style.fontSize = "3vw"
+    img.style.borderRadius = ".75vw"
+    img.style.transition = "500ms"
+}
+
+function normal(img) {
+    img.style.padding = "1vw 2vw"
+    img.style.fontSize = "2vw"
+    img.style.borderRadius = ".5vw"
+    img.style.transition = "500ms"
+}
